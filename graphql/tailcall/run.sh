172e7d3f886ee66e3c8980372e03e51a9fd1f6b2
amit <amitksingh1490@gmail.com>
2023-09-25 16:09:56 +0530

    install tc vi curl

--- graphql/tailcall/run.sh
+++ graphql/tailcall/run.sh
@@ -1,1 +1,3 @@
-../../../tailcall/target/release/tailcall start benchmark.graphql
+curl -sSL https://raw.githubusercontent.com/tailcallhq/tailcall/main/install.sh | bash -s -- v0.9.0
+export PATH=$PATH:~/.tailcall/bin
+tc start benchmark.graphql
