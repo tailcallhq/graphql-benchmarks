e14a8369e790576f29d7c7da3c136b57d6823448
Anuraag Marapally <47111459+A-N-uraag@users.noreply.github.com>
2023-11-17 17:03:28 +0530

    Updated all graphql servers to use same port (#11)
    * Updated all graphql servers to use same port
    
    * Reworked bench.sh script and disabled graphiql for tailcall

--- graphql/apollo_server/index.js
+++ graphql/apollo_server/index.js
@@ -56,7 +56,7 @@ const resolvers = {
 const server = new ApolloServer({typeDefs, resolvers});
 
 const { url } = await startStandaloneServer(server, {
-  listen: { port: 8080 },
+  listen: { port: 8000 },
 });
 
 console.log(`🚀  Server ready at: ${url}`);
