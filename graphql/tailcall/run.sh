8814f829ffbdba30261103c7b882081f5af08ec5
Amit Singh <amitksingh1490@gmail.com>
2023-11-18 13:42:13 +0530

    Update run.sh

--- graphql/tailcall/run.sh
+++ graphql/tailcall/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 current_dir=$(pwd)
-$HOME/.tailcall/bin/tailcall start $current_dir/graphql/tailcall/benchmark.graphql --log-level= error
+$HOME/.tailcall/bin/tailcall start $current_dir/graphql/tailcall/benchmark.graphql --log-level error
