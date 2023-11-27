561d42aea928cd8aeb514a31b21c1fb7f656e4ba
Saïd <20957603+ayewo@users.noreply.github.com>
2023-11-27 10:15:49 +0100

    Tidy up and remove unused code

--- graphql/netflix_dgs/src/main/java/com/example/netflixdgs/PostsDataFetcher.java
+++ graphql/netflix_dgs/src/main/java/com/example/netflixdgs/PostsDataFetcher.java
@@ -1,14 +1,5 @@
 package com.example.netflixdgs;
 
-//import java.net.URI;
-//import java.util.stream.Collectors;
-//import org.springframework.stereotype.Service;
-//import org.springframework.web.reactive.function.client.WebClient;
-//import reactor.core.publisher.Mono;
-//import reactor.netty.http.client.HttpClient;
-//import reactor.netty.transport.ProxyProvider;
-//import org.springframework.http.client.reactive.ReactorClientHttpConnector;
-
 import java.util.List;
 
 import com.netflix.graphql.dgs.DgsComponent;
@@ -21,37 +12,12 @@ import org.springframework.http.ResponseEntity;
 
 @DgsComponent
 public class PostsDataFetcher {
-	// private final WebClient webClient;
   private final RestTemplate restTemplate;
 
  	@Autowired
-  public PostsDataFetcher(RestTemplate restTemplate) {
-    this.restTemplate = restTemplate;
-  }
-
-
-	// @Autowired
-	// public PostsDataFetcher(WebClient.Builder webClientBuilder) {
-	// 	this.webClient = webClientBuilder
-	// 	.clientConnector(new ReactorClientHttpConnector(build()))
-	// 	.build();
-	// }
-
-// 	private HttpClient build() {
-//         HttpClient httpClient = HttpClient.create()
-// 						.proxy(proxy -> proxy.type(ProxyProvider.Proxy.HTTP)
-//                                    .host("localhost")
-//                                    .port(8080));
-//         return httpClient;
-// }
-
-	// private final User u = new User(1, "ABC", "abc", "a@b.com", "xyz", "www.something.com");
-
-	// private final List<Post> posts = List.of(
-	// 	new Post(1,1,"post 1", "abc", u),
-	// 	new Post(2,1,"post 2", "def", u),
-	// 	new Post(3,1,"post 3", "ghi", u)
-	// );
+  	public PostsDataFetcher(RestTemplate restTemplate) {
+    	this.restTemplate = restTemplate;
+  	}
 
 	@DgsQuery
 	public List<Post> posts() {
@@ -66,17 +32,6 @@ public class PostsDataFetcher {
 
 		return responseEntity.getBody();
 				
-		// try {
-		// 	Mono<List<Post>> responseBody = webClient
-		// 		.get()
-		// 		.uri(new URI("http://jsonplaceholder.typicode.com/posts"))
-		// 		.retrieve()
-		// 		.bodyToMono(new ParameterizedTypeReference<List<Post>>() {});
-		// 	return responseBody.block();
-		// } catch (Exception e) {
-		// 	System.out.println(e.toString());
-		// 	return List.of();
-		// }
 	}
 }
 
