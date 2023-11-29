2b47622a6e48e6851a0a37e76ac89f4ea221932e
Saïd <20957603+ayewo@users.noreply.github.com>
2023-11-29 21:27:34 +0100

    Add proxy route

--- graphql/netflix_dgs/src/main/java/com/example/netflixdgs/AppConfig.java
+++ graphql/netflix_dgs/src/main/java/com/example/netflixdgs/AppConfig.java
@@ -9,6 +9,7 @@ import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
+import org.apache.hc.client5.http.impl.routing.DefaultProxyRoutePlanner;
 import org.apache.hc.core5.http.HttpHost;
 
 @Configuration
@@ -20,12 +21,13 @@ public class AppConfig {
         // the maximum number of connections. See https://github.com/tailcallhq/graphql-benchmarks/blob/main/wrk/bench.sh
         connectionManager.setMaxTotal(100); 
         connectionManager.setDefaultMaxPerRoute(20);
-
+        
         final HttpHost proxy = new HttpHost("127.0.0.1", 3000);        
+        final DefaultProxyRoutePlanner routePlanner = new DefaultProxyRoutePlanner(proxy);
         final CloseableHttpClient httpClient = HttpClients.custom()
+        		.setRoutePlanner(routePlanner)
                 .setConnectionManager(connectionManager)
                 .setConnectionManagerShared(true)
-                .setProxy(proxy)
                 .build();
         final HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient);
         return new RestTemplate(factory);
