b66ce34295fcc8643ebfd9598b3be337e7e55b78
Saïd <20957603+ayewo@users.noreply.github.com>
2023-11-29 22:10:38 +0100

    Add keep-alive strategy

--- graphql/netflix_dgs/src/main/java/com/example/netflixdgs/AppConfig.java
+++ graphql/netflix_dgs/src/main/java/com/example/netflixdgs/AppConfig.java
@@ -6,11 +6,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+
+import java.util.concurrent.TimeUnit;
+
+import org.apache.hc.client5.http.ConnectionKeepAliveStrategy;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
 import org.apache.hc.client5.http.impl.routing.DefaultProxyRoutePlanner;
 import org.apache.hc.core5.http.HttpHost;
+import org.apache.hc.core5.http.HttpResponse;
+import org.apache.hc.core5.http.protocol.HttpContext;
+import org.apache.hc.core5.util.TimeValue;
 
 @Configuration
 public class AppConfig {
@@ -22,12 +29,20 @@ public class AppConfig {
         connectionManager.setMaxTotal(100); 
         connectionManager.setDefaultMaxPerRoute(20);
         
+        final ConnectionKeepAliveStrategy keepAliveStrategy = new ConnectionKeepAliveStrategy() {
+            public TimeValue getKeepAliveDuration(HttpResponse response, HttpContext context) {
+            	// Keep alive for 5 seconds
+                return TimeValue.of(5, TimeUnit.SECONDS);
+            }
+        };
+        
         final HttpHost proxy = new HttpHost("127.0.0.1", 3000);        
         final DefaultProxyRoutePlanner routePlanner = new DefaultProxyRoutePlanner(proxy);
         final CloseableHttpClient httpClient = HttpClients.custom()
         		.setRoutePlanner(routePlanner)
                 .setConnectionManager(connectionManager)
                 .setConnectionManagerShared(true)
+                .setKeepAliveStrategy(keepAliveStrategy)
                 .build();
         final HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient);
         return new RestTemplate(factory);
