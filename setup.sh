94270ff30b0fbebcfae3b593027a25b06125b13d
amit <amitksingh1490@gmail.com>
2024-05-14 17:27:24 +0530

    chore: cleanup scripts

--- setup.sh
+++ setup.sh
@@ -15,7 +15,7 @@ cd graphql/netflix_dgs
 cd ../../
 
 # For tailcall:
-curl -sSL https://raw.githubusercontent.com/tailcallhq/tailcall/main/install.sh | bash -s -- v0.34.1
+curl -sSL https://raw.githubusercontent.com/tailcallhq/tailcall/main/install.sh | bash -s -- v0.82.19
 export PATH=$PATH:/root/.tailcall/bin
 
 # For caliban
