5d46797b8517ccd1d0dab914c2a8015a965bcbbf
Kyri Petrou <kyri.petrou@outlook.com>
2024-05-21 09:46:25 +0200

    Use default netty's default transport

--- graphql/caliban/src/main/scala/Main.scala
+++ graphql/caliban/src/main/scala/Main.scala
@@ -1,11 +1,9 @@
 import caliban.*
 import caliban.execution.QueryExecution
 import caliban.quick.*
-import io.netty.channel.kqueue.KQueue
-import io.netty.incubator.channel.uring.IOUring
 import zio.*
+import zio.http.netty.NettyConfig
 import zio.http.netty.NettyConfig.LeakDetectionLevel
-import zio.http.netty.{ChannelType, NettyConfig}
 import zio.http.{Client as _, *}
 
 object Main extends ZIOAppDefault {
@@ -18,17 +16,6 @@ object Main extends ZIOAppDefault {
       ZLayer.scoped(Configurator.setQueryExecution(QueryExecution.Batched))
     )
 
-  private val nettyConfig: UIO[NettyConfig] = {
-    if KQueue.isAvailable then ZIO.succeed(ChannelType.KQUEUE)
-    else if IOUring.isAvailable then ZIO.succeed(ChannelType.URING)
-    else ZIO.dieMessage("Expected KQueue or IOUring transport to be available")
-  }.map(
-    NettyConfig.default
-      .maxThreads(java.lang.Runtime.getRuntime.availableProcessors())
-      .leakDetection(LeakDetectionLevel.DISABLED)
-      .channelType(_)
-  )
-
   private val api = graphQL(RootResolver(Query(Service.posts)))
 
   def run =
@@ -40,6 +27,10 @@ object Main extends ZIOAppDefault {
         Client.live,
         Server.customized,
         ZLayer.succeed(Server.Config.default.port(8000)),
-        ZLayer(nettyConfig)
+        ZLayer.succeed(
+          NettyConfig.default
+            .maxThreads(java.lang.Runtime.getRuntime.availableProcessors())
+            .leakDetection(LeakDetectionLevel.DISABLED)
+        )
       )
 }
