be8e6b427504d29c37c87fb6aec4552cdcf2b6af
Anshgrover23 <168731971+Anshgrover23@users.noreply.github.com>
2024-07-03 14:29:55 +0530

    Update run_benchmarks.sh

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -69,9 +69,6 @@ for service in "apollo_server" "caliban" "netflix_dgs" "gqlgen" "tailcall" "asyn
     fi
 done
 
-bash analyze.sh "${bench1Results[@]}"
-bash analyze.sh "${bench2Results[@]}"
-
 # Run the new analyze.js script
 node analyze.js "${bench1Results[@]}"
 node analyze.js "${bench2Results[@]}"
