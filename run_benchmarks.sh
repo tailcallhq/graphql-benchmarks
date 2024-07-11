372c384e60b82daed4648fdad231a40fc6e04f9a
daveads <the.daveads@gmail.com>
2024-07-11 15:24:58 +0100

    .

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -1,27 +1,12 @@
 #!/bin/bash
 
-# run_benchmarks.sh
-
-function killServerOnPort() {
-  local port="$1"
-  local pid=$(lsof -t -i:"$port")
-
-  if [ -n "$pid" ]; then
-    kill "$pid"
-    echo "Killed process running on port $port"
-  else
-    echo "No process found running on port $port"
-  fi
-}
-
-killServerOnPort 3000
-sh nginx/run.sh
-
+# Function to run benchmark for a single service
 function runBenchmark() {
-    killServerOnPort 8000
-    sleep 5
     local serviceScript="$1"
     local benchmarks=(1 2 3)
+
+    killServerOnPort 8000
+    sleep 5
 
     if [[ "$serviceScript" == *"hasura"* ]]; then
         bash "$serviceScript" # Run synchronously without background process
@@ -36,7 +21,6 @@ function runBenchmark() {
         graphqlEndpoint=http://$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' graphql-engine):8080/v1/graphql
     fi
 
-    # Replace / with _
     local sanitizedServiceScriptName=$(echo "$serviceScript" | tr '/' '_')
 
     for bench in "${benchmarks[@]}"; do
@@ -47,25 +31,29 @@ function runBenchmark() {
 
         # Warmup run
         bash "$benchmarkScript" "$graphqlEndpoint" "$bench" >/dev/null
-        sleep 1 # Give some time for apps to finish in-flight requests from warmup
+        sleep 1
         bash "$benchmarkScript" "$graphqlEndpoint" "$bench" >/dev/null
         sleep 1
         bash "$benchmarkScript" "$graphqlEndpoint" "$bench" >/dev/null
         sleep 1
 
-        # Benchmark run
+        # Actual benchmark run
         echo "Running benchmark $bench for $serviceScript"
         bash "$benchmarkScript" "$graphqlEndpoint" "$bench" >"bench${bench}_${resultFile}"
     done
 
+    # Cleanup
     if [ "$serviceScript" == "graphql/apollo_server/run.sh" ]; then
         cd graphql/apollo_server/
         npm stop
         cd ../../
     elif [ "$serviceScript" == "graphql/hasura/run.sh" ]; then
         bash "graphql/hasura/kill.sh"
+    else
+        killServerOnPort 8000
     fi
 }
 
-# Run the benchmark for the specified service
-runBenchmark "graphql/$1/run.sh"
+# Main execution
+serviceScript="$1"
+runBenchmark "$serviceScript"
