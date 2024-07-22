6a4d4692c5e6302e4c271299d1113ec9bd7704b5
Kunam Balaram Reddy <webbdays@gmail.com>
2024-07-22 11:26:44 +0530

    final (#16)
    * benhmarking ci t3
    
    * test 1
    
    * matrix fix
    
    * trigger
    
    * fix
    
    * trigger
    
    * trigger
    
    * fix
    
    * fix
    
    * fix
    
    * trigger a workflow
    
    * trigger
    
    * trigger
    
    * fix 2
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * trigger
    
    * fix
    
    * fix
    
    * sapce update
    
    * final
    
    * final
    
    * final and last
    
    * final and last and final
    
    * final and last and final
    
    * final exam. iritated
    
    * packup
    
    * packup
    
    * test 1
    
    * test 2
    
    * test 3
    
    * krishna
    
    * krishna 1
    
    * rama
    
    * rama, krishna
    
    * ram

--- wrk/run.sh
+++ wrk/run.sh
@@ -2,7 +2,7 @@ graphqlEndpoint="${1:-http://localhost:8000/graphql}"
 whichBench=$2
 
 if [ "$whichBench" == "./wrk/benchmarks/wrk2.lua" ]; then
-    wrk -d 30 -t 4 -c 100 -s "$(pwd)/wrk/${whichBench}" "$graphqlEndpoint"
+    wrk -d 30 -t 4 -c 100 -s "$(pwd)/${whichBench}" "$graphqlEndpoint"
 else
-    wrk -d 10 -t 4 -c 100 -s "$(pwd)/wrk/${whichBench}" "$graphqlEndpoint"
+    wrk -d 10 -t 4 -c 100 -s "$(pwd)/${whichBench}" "$graphqlEndpoint"
 fi
