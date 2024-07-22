8528ef19aea480c41fb948eadc00312fb253e75a
Kunam Balaram Reddy <webbdays@gmail.com>
2024-07-22 08:51:04 +0530

    Benchmark test (#8)
    * benhmarking ci t3
    
    * test 1
    
    * matrix fix
    
    * trigger
    
    * fix
    
    * trigger
    
    * trigger
    
    * fix
    
    * fix
    
    * fix
    
    * trigger a workflow
    
    * trigger
    
    * trigger
    
    * fix 2
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * trigger
    
    * fix
    
    * fix
    
    * sapce update
    
    * final
    
    * final
    
    * final and last

--- benchmark.py
+++ benchmark.py
@@ -34,8 +34,9 @@ for benchmark in config["benchmarks"].keys():
   
   print(f"Running benchmark for candidate: ${benchmark_candidate}");
   
+  server_start_command_script = "graphql/" + benchmark_candidate + "/run.sh"; 
   ## start benchmark candidate server
-  subprocess.call([f"graphql/${benchmark_candidate}/run.sh"]);
+  subprocess.call(server_start_command_script);
 
   ## warmup the server
   print("Running warmup for candidate: ${benchmark_candidate}");
